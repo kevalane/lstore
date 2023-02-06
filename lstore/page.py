1b510be2e6ae0ab05992accfc152304078c8aed1
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2023-02-06 15:46:40 -0800

    implemented page with bytearray appending on write

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,32 @@
+import struct
+
+DATA_SIZE = 8   # int64
+PAGE_SIZE = 4096
+STORAGE_OPTION = 'big'
 
 class Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray(PAGE_SIZE)
 
     def has_capacity(self):
-        pass
+        return self.num_records*DATA_SIZE < PAGE_SIZE
 
     def write(self, value):
+        # convert value to bytes
+        # https://stackoverflow.com/questions/6187699/how-to-convert-integer-value-to-array-of-four-bytes-in-python
+        value_to_bytes = value.to_bytes(DATA_SIZE, 
+                                        byteorder=STORAGE_OPTION, 
+                                        signed=False)
+
+        # calc offset
+        offset = self.num_records * DATA_SIZE
+
+        # write the value to the page
+        self.data[offset:offset+DATA_SIZE] = value_to_bytes
+
+        # increment num_records
         self.num_records += 1
-        pass
+
 
