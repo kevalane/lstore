a0272a502ea1fc2872e004e0f183ae26375c213e
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2023-02-09 23:31:16 -0800

    made a hashmap of all tables, added getter, must be unit tested though

--- lstore/db.py
+++ lstore/db.py
@@ -3,7 +3,7 @@ from lstore.table import Table
 class Database():
 
     def __init__(self):
-        self.tables = []
+        self.tables = {} # hashtable for O(1) delete / get
         pass
 
     # Not required for milestone1
@@ -21,6 +21,7 @@ class Database():
     """
     def create_table(self, name, num_columns, key_index):
         table = Table(name, num_columns, key_index)
+        self.tables[str(table.name)] = table
         return table
 
     
@@ -35,4 +36,7 @@ class Database():
     # Returns table with the passed name
     """
     def get_table(self, name):
-        pass
+        if (self.tables[str(name)]):
+            return self.tables[str(name)]
+        else:
+            return -1
