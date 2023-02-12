77c8bac5714ccea456fc2d39ac9dc64fab1b375f
RoPrasad <41776021+RoPrasad@users.noreply.github.com>
2023-02-11 17:04:59 -0800

    Update index.py

--- lstore/index.py
+++ lstore/index.py
@@ -9,26 +9,61 @@ class Index:
         self.indices = [None] *  table.num_columns
         pass
 
-    """
-    # returns the location of all records with the given value on column "column"
-    """
-
+    
     def locate(self, column, value):
-        pass
+        #given a value and column number, go through the corresponding index dictionary and 
+        #find the value 
+        #if you find the value, return the corresponding key list
+        #else, return an empty list
+        if column in indices: #indicates whether index already made for given column
+            working_index = indices.get(column) #gets the index associated with the column number
+            RID_list = working_index.get(value,[]) #gets the keys associated with the value
+
+        else:
+            self.create_index(column)
+            working_index = indices.get(column) #gets the index associated with the column number
+            RID_list = working_index.get(value,[]) #gets the keys associated with the value
+
+        return RID_list 
+
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
     def locate_range(self, begin, end, column):
+        range_RID_list = []
+        for i in range[begin,end]:
+            range_RID_list.append(self.locate(column,i))
+
+        return range_RID_list
+        #given a range of values and a column number, go through the corresponding column index
+        #dict and serach for the range of values
+        #initiate an empyty list in the beginning to store all the key lists per value, append
+        #after each value is searched for 
+        
         pass
 
     """
     # optional: Create index on specific column
     """
+indices = {}
 
     def create_index(self, column_number):
-        pass
+        #for column number, create an empty dictionary. go through records, 
+        #store column value as value, RID column as key list. loop through each record 
+        #and append to key list if value already exists, else create new dictionary entry
+        index = {}
+        for record in Table:
+            value = Table[column_number]
+            RID = Table[0]
+            if value in index:
+                index[value].append(RID)
+            else:
+                index[value] = [RID]
+        indices[column_number] = index
+        
+        return indices
 
     """
     # optional: Drop index of specific column
