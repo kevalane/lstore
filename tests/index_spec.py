6ec5a7c1a7cc3da7e711a50987128e97c040402c
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2023-02-12 18:46:49 -0800

    added push_record_to_index test case

--- tests/index_spec.py
+++ tests/index_spec.py
@@ -8,25 +8,32 @@ class IndexTestCase(unittest.TestCase):
         index = Index(table)
         self.assertEquals(index.indices, {})
 
-    def test_create_index(self):
+    def setup(self):
         table = Table("test", 3, 0)
         index = Index(table)
         col = 0
         self.assertTrue(index.create_index(col))
         self.assertFalse(index.create_index(col))
         self.assertEquals(index.indices[col], {})
+        return index, table
+
+    def test_create_index(self):
+        self.setup()
 
     def test_push_record_to_index(self):
         # setup
-        table = Table("test2", 3, 0)
-        index = Index(table)
-        col = 0
-        index.create_index(col)
-        self.assertTrue(index.create_index(col))
-        self.assertFalse(index.create_index(col))
-        self.assertEquals(index.indices[col], {})
+        index, col = self.setup()
 
         # test
         record = Record(0, 0, [0, 1, 2])
+        record2 = Record(1, 0, [0, 33, 2])
+        record3 = Record(22, 0, [55, 33, 2])
         index.push_record_to_index(record)
-        self.assertEquals(index.indices[col][1], [0])
+        index.push_record_to_index(record2)
+        index.push_record_to_index(record3)
+        self.assertEquals(index.indices[0][0], [0, 1])
+        self.assertEquals(index.indices[0][55], [22])
+        self.assertEquals(index.indices[1][33], [1, 22])
+        self.assertEquals(index.indices[2][2], [0, 1, 22])
+
+    
