c023299c9dc3f41823dae34f58f40231c71b742b
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2023-02-12 18:57:37 -0800

    added coverage report|

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -1,5 +1,13 @@
 import unittest
+import coverage
 
 if __name__ == '__main__':
+    cov = coverage.Coverage()
+    cov.start()
+
     test_suite = unittest.defaultTestLoader.discover('tests', pattern='*_spec.py')
     unittest.TextTestRunner().run(test_suite)
+
+    cov.stop()
+    cov.save()
+    cov.report()
