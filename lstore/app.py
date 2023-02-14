2e8afbbfc406a8d1b746964b05c679d3beb6fb38
risjainucd <64342146+risjainucd@users.noreply.github.com>
2023-02-14 15:47:56 -0800

    Skeleton Code for UI

---
+++ lstore/app.py
@@ -0,0 +1,63 @@
+from ast import Delete
+import streamlit as st
+# from query import Query
+
+def insert_record(key, val1, val2, val3, val4):
+    # Query.insert(key, val1, val2, val3, val4)
+    st.write("Record with key {} inserted successfully.".format(key))
+    pass
+
+def update_record(key, val1, val2, val3, val4):
+    # Query.update(key,val1, val2, val3, val4)
+    st.write("Record with key {} updated successfully.".format(key))
+    pass
+
+def delete_record(key):
+    st.write("Record with key {} deleted successfully.".format(key))
+    # Query.delete(key)
+    pass
+
+def sum_data(start_key, end_key):
+    # Query.sum(start_key,end_key)
+    pass
+
+# def display_table(data):
+#     st.write("| key | val1 | val2 | val3 | val4 |")
+#     st.write("|:---:|:----:|:----:|:----:|:----:|")
+#     for key in data:
+#         st.write("| {} | {} | {} | {} | {} |".format(key, data[key]['val1'], data[key]['val2'], data[key]['val3'], data[key]['val4']))
+
+def main():
+    st.title("DBMS Demo")
+    
+    operation = st.selectbox("Select Operation", ["Select", "Insert", "Update", "Delete", "Sum"])
+    
+    if operation == "Insert" or operation == "Update":
+        key = st.number_input("Enter key:")
+        val1 = st.number_input("Enter value 1:")
+        val2 = st.number_input("Enter value 2:")
+        val3 = st.number_input("Enter value 3:")
+        val4 = st.number_input("Enter value 4:")
+
+    elif operation == "Delete":
+       key = st.number_input("Enter key:") 
+       
+    elif operation == "Sum":
+        start_range = st.number_input("Enter Start Key")
+        end_range = st.number_input("Enter End Key")
+             
+    if st.button("Execute"):
+        if operation == "Insert":
+            insert_record(key, val1, val2, val3, val4)
+            
+        elif operation == "Update":
+            update_record(key, val1, val2, val3, val4)
+            
+        elif operation == "Delete":
+            delete_record(key)
+            
+        elif operation == "Sum":
+            sum_data(start_range,end_range)
+
+if __name__ == "__main__":
+    main()
