b0cb3f1b415bf9012d0b253a85be62aa34cb3ab9
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2023-02-14 22:24:38 -0800

    moved around comments

--- lstore/page.py
+++ lstore/page.py
@@ -6,31 +6,32 @@ STORAGE_OPTION  =    'big'
 Class to represent a page in the database
 """
 class Page:
-    """
-    Constructor for the Page class
-    Initializes the number of records to 0 and the data 
-    to a bytearray of size PAGE_SIZE, where PAGE_SIZE is
-    a constant number of bytes.
-    """
+
     def __init__(self) -> None:
+        """
+        Constructor for the Page class
+        Initializes the number of records to 0 and the data 
+        to a bytearray of size PAGE_SIZE, where PAGE_SIZE is
+        a constant number of bytes.
+        """
         self.num_records = 0
         self.data = bytearray(PAGE_SIZE)
 
-    """
-    @returns    True if there is space to insert a record, 
-                False otherwise
-    """
     def has_capacity(self) -> bool:
+        """
+        @returns    True if there is space to insert a record, 
+                    False otherwise
+        """
         return self.num_records*DATA_SIZE < PAGE_SIZE
 
-    """
-    Writes a value to the page
-    :param  value: int64        The value to write to the page
-
-    @returns    True if the value was written to the page,
-                False otherwise
-    """
     def write(self, value: int) -> bool:
+        """
+        Writes a value to the page
+        :param  value: int64        The value to write to the page
+
+        @returns    True if the value was written to the page,
+                    False otherwise
+        """
         if not self.has_capacity():
             return False
         
@@ -41,15 +42,15 @@ class Page:
 
         return True
 
-    """
-    Updates a value in the page on a given offset
-    :param  value: int64        The value to write to the page
-    :param  offset: int         The offset to write the value to
-
-    @returns    True if the value was written to the page,
-                False otherwise
-    """
     def put(self, value: int, offset: int) -> bool:
+        """
+        Updates a value in the page on a given offset
+        :param  value: int64        The value to write to the page
+        :param  offset: int         The offset to write the value to
+
+        @returns    True if the value was written to the page,
+                    False otherwise
+        """
         if not self._valid_offset(offset):
             return False
 
@@ -57,24 +58,24 @@ class Page:
 
         return True
 
-    """
-    Deletes a value in the page on a given offset (sets to 0)
-    :param  offset: int         The offset to write the value to
-
-    @returns    True if the value 0 was written to the given field,
-                False otherwise
-    """
     def delete(self, offset: int) -> bool:
+        """
+        Deletes a value in the page on a given offset (sets to 0)
+        :param  offset: int         The offset to write the value to
+
+        @returns    True if the value 0 was written to the given field,
+                    False otherwise
+        """
         return self.put(0, offset)
 
-    """
-    Gets value stored in the page at the given offset
-    :param  offset: int         The offset to check
-
-    @returns    The value stored in the page at the given offset,
-                -1 if the offset is invalid
-    """
     def get(self, offset: int) -> int:
+        """
+        Gets value stored in the page at the given offset
+        :param  offset: int         The offset to check
+
+        @returns    The value stored in the page at the given offset,
+                    -1 if the offset is invalid
+        """
         if not self._valid_offset(offset):
             return -1
 
@@ -88,13 +89,12 @@ class Page:
 
         return value
     
-
-    """
-    Inserts and converts value into the page at the given offset
-    :param  value: int64        The value to write to the page
-    :param  offset: int         The offset to write the value to
-    """
     def _insert(self, value: int, offset: int) -> None:
+        """
+        Inserts and converts value into the page at the given offset
+        :param  value: int64        The value to write to the page
+        :param  offset: int         The offset to write the value to
+        """
         # convert value to bytes
         value_to_bytes = value.to_bytes(DATA_SIZE, 
                                         byteorder=STORAGE_OPTION, 
@@ -106,13 +106,13 @@ class Page:
         # write the value to the page
         self.data[byte_offset:byte_offset+DATA_SIZE] = value_to_bytes
 
-    """
-    Checks if the given offset is valid
-    :param  offset: int         The offset to write the value to
-
-    @returns   True if the offset is valid, False otherwise
-    """
     def _valid_offset(self, offset: int) -> bool:
+        """
+        Checks if the given offset is valid
+        :param  offset: int         The offset to write the value to
+
+        @returns   True if the offset is valid, False otherwise
+        """
         if offset < 0 or offset >= PAGE_SIZE/DATA_SIZE:
             return False # out of bounds offset
 
--- lstore/record.py
+++ lstore/record.py
@@ -1,15 +1,38 @@
 class Record:
 
-    def __init__(self, key, columns, rid=None):
+    def __init__(self, key: int, columns: list[int], rid=None):
+        """
+        Constructor for the Record class.
+        
+        :param  key:        int             The key for the record
+        :param  columns:    List[int]       The columns of the record
+        :param  rid:        int, optional   The record ID, defaults to None
+        """
         self.rid = rid
         self.key = key
         self.columns = columns
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> int:
+        """
+        Returns the column value at the given index.
+        
+        :param  index:      int             The index of the column to retrieve
+        :returns:           int             The value of the column at the given index
+        """
         return self.columns[index]
 
-    def __get__(self):
+    def __get__(self) -> list[int]:
+        """
+        Returns the columns of the record.
+        
+        :returns:           List[int]       The columns of the record
+        """
         return self.columns
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the record.
+        
+        :returns:           str             A string representation of the record
+        """
         return str(self.columns)
