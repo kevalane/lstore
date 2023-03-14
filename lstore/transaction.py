d94202054a1c0df588e00bb9b77bb5376c0d2be5
Katya Yaroslavtseva <katyayaro@Katyas-MacBook-Air-2.local>
2023-03-14 02:10:24 -0700

    abort in transaction added

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -1,5 +1,8 @@
 from lstore.table import Table, Record
 from lstore.index import Index
+from lstore.query import Query
+
+from lstore.config import *
 
 class Transaction:
 
@@ -8,6 +11,8 @@ class Transaction:
     """
     def __init__(self):
         self.queries = []
+        self.tables = []
+        self.success = 0
         pass
 
     """
@@ -19,6 +24,7 @@ class Transaction:
     """
     def add_query(self, query, table, *args):
         self.queries.append((query, args))
+        self.tables.append(table)
         # use grades_table for aborting
 
         
@@ -26,14 +32,52 @@ class Transaction:
     def run(self):
         for query, args in self.queries:
             result = query(*args)
+            
             # If the query has failed the transaction should abort
             if result == False:
                 return self.abort()
+            
+            self.success += 1
+            
         return self.commit()
 
     
     def abort(self):
-        #TODO: do roll-back and any other necessary operations
+        count = 0
+        
+        for query, args in self.queries:
+            if self.success == count:
+                return False
+            
+            if 'Query.delete' in str(query):
+                try:
+                    self.tables[count].delete_record(-args[0])
+                except:
+                    continue
+            
+            elif 'Query.insert' in str(query):
+                try:
+                    self.tables[count].delete_record(args[0])
+                except:
+                    continue
+                
+            elif 'Query.update' in str(query):
+                try:
+                    page_type, page_num, offset = self.tables[count].page_directory[args[0]]
+                    page = self.tables[count].bufferpool.retrieve_page(page_num, (page_type == 'base'), self.tables[count].num_columns)
+                    
+                    latest_tail_rid = page.columns[INDIRECTION_COLUMN].get(offset)
+                    page_type, t_page_num, t_offset = self.tables[count].page_directory[latest_tail_rid]
+                    
+                    tail_page = self.tables[count].bufferpool.retrieve_page(t_page_num, (page_type == 'base'), self.tables[count].num_columns)
+                    
+                    page.columns[INDIRECTION_COLUMN].put(tail_page.columns[INDIRECTION_COLUMN].get(offset), offset)
+                    
+                    self.tables[count].bufferpool.base_pages[page_num]['wide_page'] = page
+                except:
+                    continue
+                
+            count += 1
         return False
 
     
